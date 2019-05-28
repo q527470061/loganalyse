23f3cacf242e936504acf52fadff0077c51407d6
qin <527470061@qq.com>
2019-05-28 14:13:18 +0800

    sdf

--- src/main/java/com/yozo/loganalyse/LoganalyseApplication.java
+++ src/main/java/com/yozo/loganalyse/LoganalyseApplication.java
@@ -1,9 +1,11 @@
 package com.yozo.loganalyse;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
+@MapperScan(basePackages = "com.yozo.loganalyse.mapper")
 @PropertySource(value = {"classpath:logParttern.properties"})
 @SpringBootApplication
 public class LoganalyseApplication {
