bd15e068e83bfc203e864e2b840cfdb318d9230a
qin <527470061@qq.com>
2019-05-28 14:13:03 +0800

    sdf

---
+++ src/main/java/com/yozo/loganalyse/pojo/OperateRecord.java
@@ -0,0 +1,75 @@
+package com.yozo.loganalyse.pojo;
+
+import java.util.Date;
+
+public class OperateRecord {
+    private Long id;
+
+    private String userId;
+
+    private String app;
+
+    private String ip;
+
+    private Long onlineTime;
+
+    private String area;
+
+    private Date lastAccessTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId == null ? null : userId.trim();
+    }
+
+    public String getApp() {
+        return app;
+    }
+
+    public void setApp(String app) {
+        this.app = app == null ? null : app.trim();
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip == null ? null : ip.trim();
+    }
+
+    public Long getOnlineTime() {
+        return onlineTime;
+    }
+
+    public void setOnlineTime(Long onlineTime) {
+        this.onlineTime = onlineTime;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area == null ? null : area.trim();
+    }
+
+    public Date getLastAccessTime() {
+        return lastAccessTime;
+    }
+
+    public void setLastAccessTime(Date lastAccessTime) {
+        this.lastAccessTime = lastAccessTime;
+    }
+}
