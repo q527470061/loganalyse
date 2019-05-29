92eb559a8d3d3987650c03434167bb5a31a3e207
q527470061 <527470061@qq.com>
2019-05-29 14:33:44 +0800

    java日志分析

---
+++ src/main/java/com/yozo/loganalyse/commons/cons/CommonConstant.java
@@ -0,0 +1,10 @@
+package com.yozo.loganalyse.commons.cons;
+
+public interface CommonConstant {
+
+    // 读取日志文件行数存24小时
+    int LOGFileEXPIRETIME=86400000;
+
+    //读取用户在线统计时间戳
+    int ONLINEUNITTIME=1800000;
+}
